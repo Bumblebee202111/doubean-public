9128d5b2d7909b8efdaf6835d9a85abc0faf81d0
Beacon <1264929989@qq.com>
2023-04-29 14:10:18 +0800

    Migrate \(directly translate\) the whole project into Kotlin - Apply IDE-hinted optimizations - Fix tons of nullability risks - Also use Kotlin Gradle DSL now - Update .gitignore
    GitOrigin-RevId: fae5f0050a25a4cd7567017e077035a66ea576ca

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    id("androidx.navigation.safeargs")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk=33
+    buildFeatures {
+        dataBinding = true
+    }
+    defaultConfig {
+        applicationId = "com.doubean.ford"
+        minSdk=27
+        targetSdk=31
+        versionCode=1
+        versionName="0.4.0"
+        setProperty("archivesBaseName", "doubean_$versionName")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += "room.schemaLocation" to "$projectDir/schemas"
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+
+dependencies {
+    implementation ("androidx.appcompat:appcompat:${rootProject.extra.get("appCompatVersion")}")
+    implementation ("androidx.constraintlayout:constraintlayout:${rootProject.extra.get("constraintLayoutVersion")}")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra.get("lifecycleVersion")}")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:${rootProject.extra.get("lifecycleVersion")}")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra.get("lifecycleVersion")}")
+    implementation ("androidx.navigation:navigation-fragment:${rootProject.extra.get("navigationVersion")}")
+    implementation ("androidx.navigation:navigation-ui:${rootProject.extra.get("navigationVersion")}")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:${rootProject.extra.get("swipeRefreshLayoutVersion")}")
+    implementation ("androidx.preference:preference-ktx:${rootProject.extra.get("preferenceVersion")}")
+    implementation ("androidx.room:room-runtime:${rootProject.extra.get("roomVersion")}")
+    implementation ("androidx.webkit:webkit:${rootProject.extra.get("webkitVersion")}")
+    implementation ("androidx.work:work-runtime:${rootProject.extra.get("workVersion")}")
+    implementation ("androidx.viewpager2:viewpager2:${rootProject.extra.get("viewPagerVersion")}")
+    implementation ("commons-codec:commons-codec:${rootProject.extra.get("commonsCodecVersion")}")
+    implementation ("com.github.bumptech.glide:glide:${rootProject.extra.get("glideVersion")}")
+    implementation ("com.github.zjupure:webpdecoder:2.3.${rootProject.extra.get("glideVersion")}")
+    implementation ("com.google.android.material:material:${rootProject.extra.get("materialVersion")}")
+    implementation ("com.google.code.gson:gson:${rootProject.extra.get("gsonVersion")}")
+    implementation ("com.squareup.retrofit2:converter-gson:${rootProject.extra.get("retrofitVersion")}")
+    implementation ("com.squareup.retrofit2:retrofit:${rootProject.extra.get("retrofitVersion")}")
+
+    kapt ("androidx.room:room-compiler:${rootProject.extra.get("roomVersion")}")
+
+    annotationProcessor ("com.github.bumptech.glide:compiler:${rootProject.extra.get("glideVersion")}")
+
+    testImplementation ("junit:junit:${rootProject.extra.get("junitVersion")}")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:${rootProject.extra.get("espressoVersion")}")
+}
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+
+buildscript {
+    
+    extra.apply {
+        
+        set("compileSdkVersion", 31)
+        set("minSdkVersion", 27)
+        set("targetSdkVersion", 31)
+
+        
+        set("appCompatVersion", "1.6.1")
+        set("commonsCodecVersion", "1.15")
+        set("constraintLayoutVersion", "2.1.4")
+        set("coreTestingVersion", "2.1.0")
+        set("coroutinesVersion", "1.4.2")
+        set("espressoVersion", "3.4.0")
+        set("fragmentVersion", "1.5.6")
+        set("glideVersion", "4.14.2")
+        set("gradleVersion", "7.3.1")
+        set("gsonVersion", "2.9.1")
+        set("junitVersion", "4.13.2")
+        set("lifecycleVersion", "2.5.1")
+        set("materialVersion", "1.6.0")
+        set("navigationVersion", "2.5.3")
+        set("okhttpLoggingVersion", "4.10.0")
+        set("preferenceVersion", "1.2.0")
+        set("retrofitVersion", "2.9.0")
+        set("roomVersion", "2.5.1")
+        set("runnerVersion", "1.0.1")
+        set("swipeRefreshLayoutVersion", "1.1.0")
+        set("truthVersion", "0.42")
+        set("testExtJunit", "1.1.3")
+        set("uiAutomatorVersion", "2.2.0")
+        set("viewPagerVersion", "1.1.0-beta01")
+        set("webkitVersion", "1.6.0")
+        set("workVersion", "2.7.1")
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${rootProject.extra.get("gradleVersion")}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${rootProject.extra.get("navigationVersion")}")
+
+    }
+}
+
+tasks.register("clean").configure{
+    delete(rootProject.buildDir)
+}
