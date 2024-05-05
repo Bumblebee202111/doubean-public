4b9c614e753e0b19ed67ef543d73c3108caf6d15
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-05-05 19:18:16 +0800

    Add missing WorkManager and hilt related dependencies
    GitOrigin-RevId: 684507434c6ae9c6acb3798a824c1d2ff3b2f3fe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
