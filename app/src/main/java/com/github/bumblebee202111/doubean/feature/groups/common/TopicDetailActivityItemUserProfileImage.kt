a2cd507b546084ab79a6edf42e35943244adb260
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-05-11 01:32:41 +0800

    Migrate all usages of Glide avatarFromUrl BindingAdapter to AsyncImage ComposeView
    GitOrigin-RevId: 4a3ca61e62136b2ba4ed1e252d4de9005c5d4d9c

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/groups/common/TopicDetailActivityItemUserProfileImage.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/groups/common/TopicDetailActivityItemUserProfileImage.kt
@@ -1,21 +1,12 @@
 package com.github.bumblebee202111.doubean.feature.groups.common
 
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
-import coil.compose.AsyncImage
 import com.github.bumblebee202111.doubean.R
+import com.github.bumblebee202111.doubean.ui.common.UserProfileImage
 
 @Composable
 fun TopicDetailActivityItemUserProfileImage(url: String?) {
-    AsyncImage(
-        model = url,
-        contentDescription = null,
-        modifier = Modifier
-            .size(dimensionResource(id = R.dimen.icon_size_small))
-            .clip(CircleShape)
-    )
+    UserProfileImage(url = url, size = dimensionResource(id = R.dimen.icon_size_small))
+
 }
--- app/src/main/java/com/github/bumblebee202111/doubean/feature/notifications/NotificationAdapter.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/notifications/NotificationAdapter.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.res.dimensionResource
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.github.bumblebee202111.doubean.R
 import com.github.bumblebee202111.doubean.databinding.ListItemPostNotificationBinding
 import com.github.bumblebee202111.doubean.model.RecommendedPostNotificationItem
+import com.github.bumblebee202111.doubean.ui.common.UserProfileImage
 
 class NotificationAdapter(private val onItemClick: (notification: RecommendedPostNotificationItem) -> Unit) :
     PagingDataAdapter<RecommendedPostNotificationItem, NotificationAdapter.ViewHolder>(
@@ -34,9 +37,18 @@ class NotificationAdapter(private val onItemClick: (notification: RecommendedPos
     class ViewHolder(private val binding: ListItemPostNotificationBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: RecommendedPostNotificationItem, cardOnClickListener: View.OnClickListener) {
-            binding.post = item
-            binding.card.setOnClickListener(cardOnClickListener)
-            binding.executePendingBindings()
+            binding.apply {
+                post = item
+                
+                groupAvatar.setContent {
+                    UserProfileImage(
+                        url = item.group.avatarUrl,
+                        size = dimensionResource(id = R.dimen.icon_size_extra_small)
+                    )
+                }
+                card.setOnClickListener(cardOnClickListener)
+                executePendingBindings()
+            }
         }
 
     }
