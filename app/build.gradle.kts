8e0f8e67a6d79fa7d0bf9ea8defbff188cfbdf62
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-06-19 01:14:33 +0800

    Update dependencies, AGP & Gradle
    GitOrigin-RevId: 6551d7dc4957e6544df1324926314c27b8af44ed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     defaultConfig {
