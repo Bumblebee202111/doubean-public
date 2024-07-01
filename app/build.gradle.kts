3589bfac38adf976458a5ce77e7cc1a1697b5355
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-07-01 19:28:26 +0800

    Bump app version to 0.6.0(600)
    GitOrigin-RevId: 41b50e7399aa9ec30f9243d62dc344eb0c053f41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.github.bumblebee202111.doubean"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.5.10"
+        versionCode = 600
+        versionName = "0.6.0"
         setProperty("archivesBaseName", "doubean_$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
