146a1c2877873226cc86d787908798bd55143488
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-07-12 09:33:07 +0800

    Profile & Login: Slightly improve UX
    GitOrigin-RevId: da94a7d2891eba8b0e3b1d4b5403832e07473251

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/login/LoginFragment.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/login/LoginFragment.kt
@@ -8,19 +8,25 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.core.net.toUri
@@ -111,80 +117,102 @@ class LoginFragment : Fragment() {
         Toast.makeText(requireContext(), "Login failed", Toast.LENGTH_SHORT).show()
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun LoginScreen(
         viewModel: LoginViewModel = this@LoginFragment.viewModel,
     ) {
         
-        Column(Modifier.fillMaxSize()) {
-            Text(
-                text = "Login",
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Text(
-                text = "Manually sync login session from douban app preferences",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = "Note accessing the required preference usually requires root permission.\n" +
-                        "Manual syncing is highly discouraged since\n" +
-                        "1. It is poorly designed and you may meet weird bugs\n" +
-                        "2. It requires many steps and extra care\n" +
-                        "Sync can be done automatically at app startup if root is granted",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            var pref by remember { mutableStateOf("") }
-            OutlinedTextField(
-                value = pref,
-                onValueChange = { pref = it },
-                Modifier.fillMaxWidth(),
-                label = { Text("key_current_account_info pref line") },
-                supportingText = {
-                    Text(
-                        "Path: /data/data/com.douban.frodo/shared_prefs/com.douban.frodo_preferences.xml\n" +
-                                "Format:    <string name=\"key_current_account_info\">...</string>"
-                    )
-                },
-                maxLines = 5,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
-            )
-            Button(
-                onClick = { viewModel.loginWithDoubanSession(pref) },
-                Modifier.fillMaxWidth(),
-            ) {
-                Text(text = "Login")
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text(text = "Login") })
             }
-            HorizontalDivider()
-            Text(
-                text = "Login",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                text = "Work in progress, not open", style = MaterialTheme.typography.titleMedium
-            )
-            OutlinedTextField(
-                value = viewModel.phoneNumber,
-                onValueChange = viewModel::updatePhoneNumber,
-                label = { Text("Phone number") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
-            )
-
-            TextField(
-                value = viewModel.password,
-                onValueChange = viewModel::updatePassword,
-                label = { Text("Enter password") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-            )
-            Button(
-                onClick = viewModel::login,
-                Modifier.fillMaxWidth(),
-                
-                enabled = false
+        ) { innerPadding ->
+            Column(
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
             ) {
-                Text(text = "Login")
+                Text(
+                    text = "Manually sync login session from douban app preferences",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = "Note accessing the required preference usually requires root permission.\n" +
+                            "Manual syncing is highly discouraged since\n" +
+                            "1. The feature is not carefully designed and you may meet weird bugs\n" +
+                            "2. It requires many steps and extra care\n" +
+                            "Sync usually can be done automatically at app startup if root is granted",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                var pref by remember { mutableStateOf("") }
+
+                val keyboardController = LocalSoftwareKeyboardController.current
+                OutlinedTextField(
+                    value = pref,
+                    onValueChange = { pref = it },
+                    Modifier.fillMaxWidth(),
+                    label = { Text("key_current_account_info pref line") },
+                    supportingText = {
+                        Text(
+                            "Path: /data/data/com.douban.frodo/shared_prefs/com.douban.frodo_preferences.xml\n" +
+                                    "Format:    <string name=\"key_current_account_info\">...</string>"
+                        )
+                    },
+                    maxLines = 5,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hide()
+                        }
+                    ),
+                )
+
+                Button(
+                    onClick = {
+                        keyboardController?.hide()
+                        viewModel.loginWithDoubanSession(pref)
+                    },
+                    Modifier.fillMaxWidth(),
+                ) {
+                    Text(text = "Login")
+                }
+                HorizontalDivider()
+                Text(
+                    text = "Login with username and password",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = "Not implemented now and not very likely to be implemented in the future due to its difficulty",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                OutlinedTextField(
+                    enabled = false,
+                    value = viewModel.phoneNumber,
+                    onValueChange = viewModel::updatePhoneNumber,
+                    label = { Text("Phone number") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+                )
+
+                TextField(
+                    enabled = false,
+                    value = viewModel.password,
+                    onValueChange = viewModel::updatePassword,
+                    label = { Text("Enter password") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                )
+                Button(
+                    onClick = viewModel::login,
+                    Modifier.fillMaxWidth(),
+                    
+                    enabled = false
+                ) {
+                    Text(text = "Login")
+                }
             }
         }
+
     }
 
 }
