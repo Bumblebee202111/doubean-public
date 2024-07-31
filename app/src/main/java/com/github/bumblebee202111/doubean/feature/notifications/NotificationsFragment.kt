209b0afba6276789b48854552530954c322ede9c
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-07-31 23:25:03 +0800

    Migrate NotificationsFragment into Compose
    GitOrigin-RevId: a9c810bc94b2ea22fadfc5324b6ce95ba6393f8a

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/notifications/NotificationsFragment.kt
@@ -2,23 +2,34 @@ package com.github.bumblebee202111.doubean.feature.notifications
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.compose.content
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
 import com.github.bumblebee202111.doubean.R
-import com.github.bumblebee202111.doubean.databinding.FragmentNotificationsBinding
 import com.github.bumblebee202111.doubean.feature.groups.common.TopicItemWithGroupAndroidView
+import com.github.bumblebee202111.doubean.ui.theme.AppTheme
 import com.github.bumblebee202111.doubean.util.DEEP_LINK_SCHEME_AND_HOST
 import com.github.bumblebee202111.doubean.util.GROUP_PATH
 import com.github.bumblebee202111.doubean.util.TOPIC_PATH
@@ -26,55 +37,64 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NotificationsFragment : Fragment() {
-    private val notificationsViewModel: NotificationsViewModel by viewModels()
-    private lateinit var binding: FragmentNotificationsBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?, savedInstanceState: Bundle?,
-    ): View {
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false).apply {
-            lifecycleOwner = viewLifecycleOwner
-        }
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.apply {
-            toolbar.setOnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.action_settings -> {
-                        findNavController().navigate(R.id.nav_settings)
-                        true
-                    }
-
-                    else -> false
-                }
-            }
-            notifications.setContent {
-                val notificationPagingItems =
-                    notificationsViewModel.notifications.collectAsLazyPagingItems()
-                val navigateToTopic = { topicId: String ->
+    ) = content {
+        AppTheme {
+            NotificationsScreen(
+                viewModel = viewModel(),
+                navigateToTopic = { topicId ->
                     val request =
                         NavDeepLinkRequest.Builder.fromUri(topicId.topicDeepLinkUri()).build()
                     findNavController().navigate(request)
+                },
+                openSettings = {
+                    findNavController().navigate(R.id.nav_settings)
                 }
-                LazyColumn(Modifier.nestedScroll(rememberNestedScrollInteropConnection())) {
-                    items(
-                        notificationPagingItems.itemCount,
-                        notificationPagingItems.itemKey { it.id },
-                        notificationPagingItems.itemContentType { "notification" }) { index ->
-                        TopicItemWithGroupAndroidView(
-                            notificationPagingItems[index],
-                            navigateToTopic
-                        )
-                    }
-                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NotificationsScreen(
+    viewModel: NotificationsViewModel,
+    navigateToTopic: (topicId: String) -> Unit,
+    openSettings: () -> Unit,
+) {
+    Scaffold(topBar = {
+        TopAppBar(title = {}, actions = {
+            IconButton(onClick = openSettings) {
+                Icon(
+                    Icons.Filled.Settings, contentDescription = null
+                )
+            }
+        })
+    }) { innerPadding ->
+        val notificationPagingItems = viewModel.notifications.collectAsLazyPagingItems()
+
+        LazyColumn(
+            contentPadding = innerPadding,
+        ) {
+            item {
+                Text(
+                    text = stringResource(id = R.string.group_notifications_header),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                )
+            }
+            items(notificationPagingItems.itemCount,
+                notificationPagingItems.itemKey { it.id },
+                notificationPagingItems.itemContentType { "notification" }) { index ->
+                TopicItemWithGroupAndroidView(
+                    notificationPagingItems[index], navigateToTopic
+                )
             }
         }
     }
 }
 
-fun String.topicDeepLinkUri() =
-    "$DEEP_LINK_SCHEME_AND_HOST/$GROUP_PATH/$TOPIC_PATH/$this".toUri()
+
+fun String.topicDeepLinkUri() = "$DEEP_LINK_SCHEME_AND_HOST/$GROUP_PATH/$TOPIC_PATH/$this".toUri()
