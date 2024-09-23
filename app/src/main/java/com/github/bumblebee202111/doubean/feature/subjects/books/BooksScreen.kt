52396289e9c71d9dc2cd36a4cc65256e0b4eb78d
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-09-24 02:05:16 +0800

    Show Top 250 Books (first page of 20 items, no pagination)
    GitOrigin-RevId: 29fefcf1f1cbd2978a9dc1c0171afc053ca4fd92

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/books/BooksScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/books/BooksScreen.kt
@@ -1,4 +1,4 @@
-package com.github.bumblebee202111.doubean.feature.subjects.movies
+package com.github.bumblebee202111.doubean.feature.subjects.books
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,24 +9,23 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.bumblebee202111.doubean.ui.subjectCollection
 
 @Composable
-fun MoviesScreen(modifier: Modifier = Modifier, viewModel: MoviesViewModel = hiltViewModel()) {
+fun BooksScreen(modifier: Modifier = Modifier, viewModel: BooksViewModel = hiltViewModel()) {
     val moviesUiState by viewModel.moviesUiState.collectAsStateWithLifecycle()
-    MoviesScreen(
-        uiState = moviesUiState,
-        modifier
-    )
+    BooksScreen(uiState = moviesUiState, modifier)
 }
 
 @Composable
-fun MoviesScreen(uiState: MoviesUiState, modifier: Modifier = Modifier) {
+fun BooksScreen(uiState: BooksUiState, modifier: Modifier = Modifier) {
     when (uiState) {
-        MoviesUiState.Loading -> {
+        BooksUiState.Loading -> {
             //TODO
         }
-        MoviesUiState.Error -> {
+
+        BooksUiState.Error -> {
             //TODO
         }
-        is MoviesUiState.Success -> {
+
+        is BooksUiState.Success -> {
             LazyColumn(modifier = modifier) {
                 subjectCollection(uiState.title, uiState.items)
             }
