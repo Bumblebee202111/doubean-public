bc68f9b1ec3be884d642256714b088adb32f247e
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2024-12-19 08:58:13 +0800

    Tweak subject layouts - Apply rounded corners to subject images - Optimize paddings - Make reviews sheet higher and it won't scroll up when reaching bottom of actual content
    GitOrigin-RevId: 0fd0734c9fe5f002ef5cdb49fb2cca6ccdb0c4a0

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/book/BookScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/book/BookScreen.kt
@@ -1,19 +1,15 @@
 package com.github.bumblebee202111.doubean.feature.subjects.book
 
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.bumblebee202111.doubean.model.SubjectInterestStatus
 import com.github.bumblebee202111.doubean.model.SubjectType
 import com.github.bumblebee202111.doubean.ui.SubjectDetailHeader
-import com.github.bumblebee202111.doubean.ui.SubjectInfoReviewsModuleItemContent
+import com.github.bumblebee202111.doubean.ui.SubjectReviewsSheetContent
+import com.github.bumblebee202111.doubean.ui.SubjectScaffold
 import com.github.bumblebee202111.doubean.ui.SubjectTopBar
 import com.github.bumblebee202111.doubean.ui.subjectInfoInterestsModuleItem
 import com.github.bumblebee202111.doubean.ui.subjectInfoIntroModuleItem
@@ -33,7 +29,6 @@ fun BookScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookScreen(
     bookUiState: BookUiState,
@@ -41,18 +36,16 @@ fun BookScreen(
     onLoginClick: () -> Unit,
     onUpdateStatus: (newStatus: SubjectInterestStatus) -> Unit,
 ) {
-    BottomSheetScaffold(
-        sheetContent = {
+    SubjectScaffold(
+        reviewsSheetContent = {
             if (bookUiState is BookUiState.Success) {
-                SubjectInfoReviewsModuleItemContent(
+                SubjectReviewsSheetContent(
                     subjectType = SubjectType.BOOK,
-                    reviews = bookUiState.reviews,
-                    modifier = Modifier.fillMaxHeight(0.618f)
+                    reviews = bookUiState.reviews
                 )
 
             }
         },
-        sheetPeekHeight = 128.dp,
         topBar = {
             BookTopBar(bookUiState = bookUiState, onBackClick = onBackClick)
         }
--- app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/tv/TvScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/tv/TvScreen.kt
@@ -1,19 +1,15 @@
 package com.github.bumblebee202111.doubean.feature.subjects.tv
 
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.bumblebee202111.doubean.model.SubjectInterestStatus
 import com.github.bumblebee202111.doubean.model.SubjectType
 import com.github.bumblebee202111.doubean.ui.SubjectDetailHeader
 import com.github.bumblebee202111.doubean.ui.SubjectInfoReviewsModuleItemContent
+import com.github.bumblebee202111.doubean.ui.SubjectScaffold
 import com.github.bumblebee202111.doubean.ui.SubjectTopBar
 import com.github.bumblebee202111.doubean.ui.subjectInfoCelebritiesModuleItem
 import com.github.bumblebee202111.doubean.ui.subjectInfoInterestsModuleItem
@@ -35,7 +31,6 @@ fun TvScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TvScreen(
     tvUiState: TvUiState,
@@ -43,18 +38,16 @@ fun TvScreen(
     onLoginClick: () -> Unit,
     onUpdateStatus: (newStatus: SubjectInterestStatus) -> Unit,
 ) {
-    BottomSheetScaffold(
-        sheetContent = {
+    SubjectScaffold(
+        reviewsSheetContent = {
             if (tvUiState is TvUiState.Success) {
                 SubjectInfoReviewsModuleItemContent(
                     subjectType = SubjectType.TV,
                     reviews = tvUiState.reviews,
-                    modifier = Modifier.fillMaxHeight(0.618f)
                 )
 
             }
         },
-        sheetPeekHeight = 128.dp,
         topBar = {
             TvTopBar(tvUiState = tvUiState, onBackClick = onBackClick)
         }
