0cc9f8071c012576265c2cf014ed2c8b9109d445
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-02-09 20:24:43 +0800

    Bump version to 0.7.12(712) & update docs
    GitOrigin-RevId: 621338252deea038c26e18ec0616b76cdf360a64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.github.bumblebee202111.doubean"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 711
-        versionName = "0.7.11"
+        versionCode = 712
+        versionName = "0.7.12"
         setProperty("archivesBaseName", "doubean_$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
