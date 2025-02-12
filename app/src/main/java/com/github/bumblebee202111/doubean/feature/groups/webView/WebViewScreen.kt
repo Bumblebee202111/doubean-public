8a85dc1ebc4dfd06c50892cdf12d32df98b5df48
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-02-12 14:18:43 +0800

    Fix padding of DoubeanWebView in WebViewScreen
    GitOrigin-RevId: 0a88ca65bade722d38c2631ca15ea9f2ffc0bc92

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/groups/webView/WebViewScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/groups/webView/WebViewScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.isVisible
 import com.github.bumblebee202111.doubean.ui.common.DoubanWebViewClient
@@ -50,7 +51,12 @@ fun WebViewScreen(url: String, onBackClick: () -> Unit) {
                     isVisible = false
                 }
             },
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier.padding(
+                start = 16.dp,
+                top = innerPadding.calculateTopPadding(),
+                end = 16.dp,
+                bottom = innerPadding.calculateBottomPadding()
+            )
         ) {
             it.run {
                 webViewClient =
