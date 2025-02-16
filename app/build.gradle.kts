38c5386cf38170ca43abb65ceb877c66703ffb47
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-02-16 16:09:19 +0800

    Bump version to 0.7.13(713)
    GitOrigin-RevId: 4eb72a77e52aa9bcec72ebd0a33eadc3eed0d91f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.github.bumblebee202111.doubean"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 712
-        versionName = "0.7.12"
+        versionCode = 713
+        versionName = "0.7.13"
         setProperty("archivesBaseName", "doubean_$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
