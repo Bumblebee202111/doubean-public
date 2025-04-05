c6041d2f82f330b2e544eb34da779a3242a135bb
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-04-05 23:20:51 +0800

    Update dependencies
    GitOrigin-RevId: 408d75f928c0fb3b36e9e862df3276091e6bb70c

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/SubjectsScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/subjects/SubjectsScreen.kt
@@ -16,9 +16,9 @@ import androidx.compose.material.icons.filled.Tv
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -134,7 +134,7 @@ private fun SubjectsAppBar(currentUser: User?, onAvatarClick: () -> Unit) {
 
 @Composable
 private fun SubjectsTabRow(pagerState: PagerState) {
-    TabRow(selectedTabIndex = pagerState.currentPage) {
+    PrimaryTabRow(selectedTabIndex = pagerState.currentPage) {
         SubjectsTab.entries.forEachIndexed { index, tab ->
             val coroutineScope = rememberCoroutineScope()
             val text = stringResource(id = tab.textResId)
