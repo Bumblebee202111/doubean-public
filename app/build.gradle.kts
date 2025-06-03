4754ee209177367ae436e5bd111fbb7ad9b1198d
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-06-03 19:56:37 +0800

    Bump version to 0.9.1(90100)
    GitOrigin-RevId: 338bcaadbd568d9299ccf2b5ea8d2a290294f340

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.github.bumblebee202111.doubean"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 90004
-        versionName = "0.9.0"
+        versionCode = 90100
+        versionName = "0.9.1"
         setProperty("archivesBaseName", "doubean_$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
