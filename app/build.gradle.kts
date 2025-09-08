76a8a279fcb5e7c57a584f968b3b7591b2585cd1
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-09-08 09:52:56 +0800

    Refactor Kotlin options in build.gradle.kts
    GitOrigin-RevId: 3d2351ebea90cb45d3fa79fcdde2803f874c56d7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -57,12 +59,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs = listOf("-Xcontext-receivers")
-    }
-
     namespace = "com.github.bumblebee202111.doubean"
 }
 
@@ -127,7 +123,16 @@ dependencies {
 
 }
 
-//kotlin.compilerOptions.freeCompilerArgs.add("-Xwhen-guards")
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+        freeCompilerArgs.addAll(
+            listOf(
+                "-Xcontext-receivers"
+            )
+        )
+    }
+}
 
 fun getDoubanAccess(): String? {
     return project.findProperty("douban_access_key") as? String
