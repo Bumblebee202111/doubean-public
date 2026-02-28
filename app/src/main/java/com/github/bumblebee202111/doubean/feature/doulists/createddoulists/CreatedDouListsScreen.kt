350678141776e1890d70100d9e61ff6f84613fe5
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-02-28 16:48:12 +0800

    fix(nav): Fix minor mistakes
    GitOrigin-RevId: 989f1b6be3acc1729bed19a6ff0a228b9dd96915

--- app/src/main/java/com/github/bumblebee202111/doubean/feature/doulists/createddoulists/CreatedDouListsScreen.kt
+++ app/src/main/java/com/github/bumblebee202111/doubean/feature/doulists/createddoulists/CreatedDouListsScreen.kt
@@ -8,13 +8,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -42,7 +36,7 @@ import com.github.bumblebee202111.doubean.ui.component.doubeanTopAppBarHeight
 fun CreatedDouListsScreen(
     onBackClick: () -> Unit,
     onDouListClick: (douListId: String) -> Unit,
-    viewModel: CreatedDouListsViewModel,
+    viewModel: CreatedDouListsViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     CreatedDouListsScreen(
