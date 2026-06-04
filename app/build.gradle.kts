401d611a20edf4a58ff7799f73935f8987ca2543
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2026-06-04 22:50:19 +0800

    chore(release): Update version to 0.12.0-beta2
    GitOrigin-RevId: 5e058c2968ba86f8658391a0769b759b7bf19c41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.github.bumblebee202111.doubean"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 120001
-        versionName = "0.12.0-beta1"
+        versionCode = 120002
+        versionName = "0.12.0-beta2"
 
         setProperty("archivesBaseName", "doubean_$versionName")
 
